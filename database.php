f7c180815919c1f5a6000a3b111a73953dc50631
kopmean <73383077+kopmean@users.noreply.github.com>
2020-11-27 02:05:50 +0700

    Create database.php

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itf63.mysql.database.azure.com', 'kopsira@itf63', 'GQFvju56', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$A = $_POST['A'];
+$B = $_POST['B'];
+$C = $A+$B;
+
+
+$sql = "INSERT INTO guestbook (name , comment , link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    header("location:show.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
